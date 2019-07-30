55e116ca9e326bc17da308dad662db9d216bc9bf
Suhabe Bugrara <suhabe@bugrara.com>
2019-07-29 18:29:11 -0700

    monitor results

---
+++ kworker/src/bin/monitor.rs
@@ -0,0 +1,45 @@
+extern crate kworker;
+extern crate env_logger;
+extern crate handlebars;
+extern crate serde;
+
+use serde::Serialize;
+use handlebars::Handlebars;
+use std::error::Error;
+use std::fs::{File,create_dir_all, remove_dir_all};
+use std::env;
+
+#[derive(Serialize)]
+struct Person {
+    name: String,
+    age: i16,
+}
+
+fn main() -> Result<(), Box<dyn Error>>  {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    let handlebars = Handlebars::new();
+
+    let data = [Person {
+        name: "Ning Sun".to_string(),
+        age: 27
+    },Person {
+        name: "Ning Sun 2".to_string(),
+        age: 27
+    }];
+
+    let kdist = env::var("KDIST_HOME").expect("KDIST_HOME not set");
+    let template = format!("{}/kworker/ui/templates/monitor.hbs", kdist);
+    let gendir = format!("{}/kworker/generated", kdist);
+    let results_file = format!("{}/results.html", gendir);
+
+    remove_dir_all(&gendir).is_ok();
+    create_dir_all(&gendir).expect("Could not create generated directory");
+
+    let mut source_template = File::open(template).unwrap();
+    let mut output_file = File::create(&results_file).unwrap();
+    handlebars.render_template_source_to_write(&mut source_template, &data, &mut output_file).unwrap();
+    println!("Generated results file {}", &results_file);
+    Ok(())
+}
