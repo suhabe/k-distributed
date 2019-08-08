661452a894139a972f13059689e1608389266ec2
Suhabe Bugrara <suhabe@bugrara.com>
2019-08-08 10:57:57 -0700

    test report

--- kworker/src/bin/monitor.rs
+++ kworker/src/bin/monitor.rs
@@ -7,10 +7,12 @@ use handlebars::Handlebars;
 use std::error::Error;
 use std::fs::{File,create_dir_all, remove_dir_all};
 use std::env;
-use kworker::job::get_jobs;
+use kworker::job::*;
 use kworker::db::exec;
 use serde::Serialize;
 use chrono::{Local,Utc};
+use itertools::Itertools;
+
 
 #[derive(Serialize, Debug, Eq, Ord, PartialEq, PartialOrd)]
 struct Row {
@@ -28,6 +30,12 @@ struct Row {
     pub proved_color:String
 }
 
+#[derive(Serialize, Debug)]
+struct Pgm {
+    pub name:String,
+    pub rows:Vec<Row>
+}
+
 fn to_local_str(x: chrono::DateTime<Utc>) -> String {
     x.with_timezone(&Local).format("%Y-%m-%d %H:%M:%S").to_string()
 }
@@ -36,46 +44,48 @@ fn e() -> String {
     String::from("")
 }
 
+fn row(j: &Job) -> Row {
+    let proved;
+    if j.completed_dt.is_some() {
+        proved = Some(j.proved.is_some() && j.proved.unwrap());
+    } else {
+        proved = None;
+    }
+
+    Row {
+        completed_dt: j.completed_dt.map_or(e(), |x| to_local_str(x)),
+        request_dt: j.request_dt.map_or(e(), |x| to_local_str(x)),
+        processed_dt: j.processing_dt.map_or(e(), |x| to_local_str(x)),
+        benchmark_name: j.benchmark_name.to_owned(),
+        processing_secs: j.get_processing_secs().map_or(e(), |x| x.to_string()),
+        processing_mins: j.get_processing_secs().map_or(e(), |x| format!("{:.1}", x as f64/60.)),
+        spec_name: j.spec_name.to_owned(),
+        status_code: match j.status_code {
+            Some(c) => c.to_string(),
+            None => match j.timed_out {
+                Some(true) => String::from("Timed out"),
+                _ => e()
+            }
+        },
+        out_url: j.output_log_s3_url().unwrap_or(e()),
+        err_url: j.error_log_s3_url().unwrap_or(e()),
+        proved: proved.map_or(e(), |x| x.to_string()),
+        proved_color: proved.map_or(String::from("#ffffff"), |x| match x {
+            true => String::from("#98FB98"),
+            false => String::from("#FFCCCB")
+        })
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
     env_logger::init();
 
     let mut jobs = exec(|tx| get_jobs(tx, None));
-
     jobs.sort_by_key(|j| j.request_dt);
-
     let mut rows = Vec::new();
     for j in jobs {
-        let proved;
-        if j.completed_dt.is_some() {
-            proved = Some(j.proved.is_some() && j.proved.unwrap());
-        } else {
-            proved = None;
-        }
-
-        rows.push(Row {
-            completed_dt: j.completed_dt.map_or(e(), |x| to_local_str(x)),
-            request_dt: j.request_dt.map_or(e(), |x| to_local_str(x)),
-            processed_dt: j.processing_dt.map_or(e(), |x| to_local_str(x)),
-            benchmark_name: j.benchmark_name.to_owned(),
-            processing_secs: j.get_processing_secs().map_or(e(), |x| x.to_string()),
-            processing_mins: j.get_processing_secs().map_or(e(), |x| format!("{:.1}", x as f64/60.)),
-            spec_name: j.spec_name.to_owned(),
-            status_code: match j.status_code {
-                Some(c) => c.to_string(),
-                None => match j.timed_out {
-                    Some(true) => String::from("Timed out"),
-                    _ => e()
-                }
-            },
-            out_url: j.output_log_s3_url().unwrap_or(e()),
-            err_url: j.error_log_s3_url().unwrap_or(e()),
-            proved: proved.map_or(e(), |x| x.to_string()),
-            proved_color: proved.map_or(String::from("#ffffff"), |x| match x {
-                true => String::from("#98FB98"),
-                false => String::from("#FFCCCB")
-            })
-        })
+        rows.push(row(&j));
     }
 
     let kdist = env::var("KDIST_HOME").expect("KDIST_HOME not set");
@@ -91,10 +101,35 @@ fn main() -> Result<(), Box<dyn Error>> {
     let report_file = format!("{}/report.html", gendir);
     generate(report_template, report_file, &rows);
 
+
+    let mut jobs = exec(|tx| get_jobs(tx, None));
+    jobs.sort_by(|j1,j2| j1.benchmark_name.cmp(&j2.benchmark_name)
+        .then(j1.request_dt.cmp(&j2.request_dt)));
+
+    let cutoff = chrono::DateTime::parse_from_rfc3339("2019-08-04T00:00:00+00:00").unwrap().with_timezone(&Utc);
+    let prefix = "TEST-";
+
+    let grouped_jobs = jobs.iter()
+        .filter(|j| j.request_dt.unwrap().ge(&cutoff))
+        .filter(|j| j.benchmark_name.starts_with(prefix))
+        .group_by(|j| j.benchmark_name.to_owned());
+
+    let mut pgms = Vec::new();
+    for (key,group) in &grouped_jobs {
+        pgms.push(Pgm {
+            name: key,
+            rows: group.into_iter().map(|j| row(&j)).collect()
+        });
+    }
+
+    let tests_template = format!("{}/kworker/ui/templates/tests.hbs", kdist);
+    let tests_file = format!("{}/tests.html", gendir);
+    generate(tests_template, tests_file, &pgms);
+
     Ok(())
 }
 
-fn generate(template_path: String, output_path: String, rows: &Vec<Row>) {
+fn generate<T>(template_path: String, output_path: String, rows: &Vec<T>) where T: Serialize {
     let handlebars = Handlebars::new();
     let mut template_file = File::open(template_path).unwrap();
     let mut output_file = File::create(&output_path).unwrap();
